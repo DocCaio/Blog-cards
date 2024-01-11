1f284daae25f19a042a1bc0113d96027737fe8f5
Caio <108128119+DocCaio@users.noreply.github.com>
2024-01-11 17:05:07 -0300

    up

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,10 +1,10 @@
 import './Banner.css'
-
-export const Banner = () => {
-    // JSX
-    return (
-        <header className="banner">
-            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
-        </header>
-    )
+function Banner() {
+   return (
+    <header className='banner'>
+     <img src="/imagens/banner.png" alt="banner"/>
+     </header>
+   )
 }
+
+export default Banner;
